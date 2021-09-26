48bbb8b985f0b0e0d0c577ea792cec31fab5296c
Dmitrii <belovd90@mail.ru>
2021-09-26 22:07:04 +0300

    Нужно больше функций

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 function getRandomNumber(min, max) {
   if (max <= min) {
-    alert('Второе число должно быть больше первого числа!!!');
+    return 'Второе число должно быть больше первого числа!!!';
   }else if(min < 0){
-    alert('Числа должны быть больше или равны 0!');
+    return 'Числа должны быть больше или равны 0!';
   }
   return Math.floor(Math.random()*(max + 1 - min)) + min;
 }
@@ -10,7 +10,7 @@ getRandomNumber(10, 23);
 
 
 function lengthCheck(string, maxLengthOfString) {
-  string < maxLengthOfString ? console.log(true) : console.log(false);
+  string < maxLengthOfString ? true : false;
 }
 
 lengthCheck(130,140);

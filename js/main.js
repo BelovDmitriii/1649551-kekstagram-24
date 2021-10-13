f228d77a7002aec6bdb5877e8ad3fd9a0922abf6
Dmitrii <belovd90@mail.ru>
2021-10-14 00:41:50 +0300

    Откорректировано прошлое дз

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import './forms.js';
 import './filter.js/';
 import './photo-description.js';
 import './preview.js';
-import {photosDescription} from './util.js';
+import {photosDescription} from './data.js';
 
 // eslint-disable-next-line no-console
 console.log(photosDescription);
